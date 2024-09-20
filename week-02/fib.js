a61c832f6d995d8fe80e189fe776a5a3f3ac2154
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-09-20 21:42:00 +0800

    feat: add non-recursive method of fib.js

--- week-02/fib.js
+++ week-02/fib.js
@@ -10,6 +10,17 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2)
 }
 
+// 不用遞迴的方法
+// function fib(n) {
+//   var arr = Array(n + 1);
+//   arr[0] = 0;
+//   arr[1] = 1;
+//   for (let i = 2; i < n + 1; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//   }
+//   return arr[n];
+// }
+
 console.log(fib(0)); // 0
 console.log(fib(1)); // 1
 console.log(fib(5)); // 5
