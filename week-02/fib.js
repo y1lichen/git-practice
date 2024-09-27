7e3dc17f26eeb8c4a6b17f25be38d9b94450c9a6
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-09-27 10:59:13 +0800

    feat: add some comment to fib

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,3 +1,5 @@
+// 這個寫法不太好。如：算fib(3)時，fib(4) = fib(3)+fib(2)
+// 又fib(3) = fib(2) + fib(1)，fib(2)就要計算兩次。
 function fib(n) {
   if (n == 0) {
     // fib(0)=0
@@ -11,15 +13,15 @@ function fib(n) {
 }
 
 // 不用遞迴的方法
-// function fib(n) {
-//   var arr = Array(n + 1);
-//   arr[0] = 0;
-//   arr[1] = 1;
-//   for (let i = 2; i < n + 1; i++) {
-//     arr[i] = arr[i - 1] + arr[i - 2];
-//   }
-//   return arr[n];
-// }
+function fib2(n) {
+  var arr = Array(n + 1);
+  arr[0] = 0;
+  arr[1] = 1;
+  for (let i = 2; i < n + 1; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
 
 console.log(fib(0)); // 0
 console.log(fib(1)); // 1
