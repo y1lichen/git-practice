f5ae301761d990f28b927cf668876c6e8121f25e
Jefferson Chen <f1jefferson@yahoo.com.tw>
2024-09-27 11:12:25 +0800

    feat: improve fib.js

--- week-02/fib.js
+++ week-02/fib.js
@@ -13,6 +13,7 @@ function fib(n) {
 }
 
 // 不用遞迴的方法
+// 用一個arr暫存之前算過的，避免重複計算
 function fib2(n) {
   var arr = Array(n + 1);
   arr[0] = 0;
@@ -23,7 +24,24 @@ function fib2(n) {
   return arr[n];
 }
 
-console.log(fib(0)); // 0
-console.log(fib(1)); // 1
-console.log(fib(5)); // 5
-console.log(fib(10)); // 55
+// 改良上一個寫法，因為fib(n)只用得到fib(n-1)和fib(n-2)
+// 那就只存前兩個就好，以節省空間
+function fib3(n) {
+  if (n === 0) {
+    return 0;
+  }
+  var arr = Array(2);
+  arr[0] = 0;
+  arr[1] = 1;
+  for (let i = 2; i < n + 1; i++) {
+    cur = arr[0] + arr[1]
+    arr[0] = arr[1];
+    arr[1] = cur;
+  }
+  return arr[1];
+}
+
+console.log(fib3(0)); // 0
+console.log(fib3(1)); // 1
+console.log(fib3(5)); // 5
+console.log(fib3(10)); // 55
